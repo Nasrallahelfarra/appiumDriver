4e89b624a4e784a827a68969460c8ffb44698250
ibrahim <isharif@famcare.app>
2022-12-12 17:27:16 +0200

    zz

--- appiumtest/appium-test/main_test.dart
+++ appiumtest/appium-test/main_test.dart
@@ -3,15 +3,25 @@ import 'package:test/test.dart';
 
 void main() {
   late AppiumWebDriver driver;
-
+//dart appium-test/main_test.dart
   setUpAll(() async {
     driver = await createDriver(
         uri: Uri.parse('http://127.0.0.1:4723/wd/hub/'),
+        // desired: {
+        //   'platformName': 'ios',
+        //   'deviceName': 'iPhone 14 Pro Max',
+        //   'browserName': 'Google Chrome',
+        //   'automationName': 'xcuitest',
+        //   'reduceMotion': true,
+        // });
+
         desired: {
-          'platformName': 'android',
-          'deviceName': 'SM A515F',
-          'browserName': 'Google Chrome',
-          'automationName': 'xcuitest',
+          'platformName': 'ios',
+          'appium:automationName': 'xcuitest',
+          'deviceName': 'iPhone 14 Pro Max',
+         // 'appium:app': 'https://github.com/projectxyzio/webinar-20210630-appium-flutter-driver/blob/master/apps/app-release-with-key-no-ext.apk?raw=true',
+          'appium:appPackage': 'com.example.appiumtest',
+          'appium:fullReset': true,
           'reduceMotion': true,
         });
   });
@@ -26,7 +36,6 @@ void main() {
 
   test('connect to existing session', () async {
     var sessionId = driver.id;
-
     AppiumWebDriver newDriver = await fromExistingSession(sessionId);
     expect(await newDriver.title, 'Appium/welcome');
     expect(newDriver.id, sessionId);
